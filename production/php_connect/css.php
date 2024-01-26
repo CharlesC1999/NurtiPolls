51f3451f6034acf5835f6c7d146e742963fea731
John <john76391@gmail.com>
2024-01-26 10:34:48 +0800

    新增資料庫連接檔案

---
+++ production/php_connect/css.php
@@ -0,0 +1,26 @@
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <style>
+        .object-fit-cover {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
+        .cart-icon {
+            font-size: 1.8rem;
+        }
+
+        .cart-count {
+            font-size: 12px;
+            width: 24px;
+            height: 24px;
+            border-radius: 50%;
+            top: -5px;
+            right: -10px;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
