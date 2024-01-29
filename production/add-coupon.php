d0319bb46d5131396a081c604667984b4d2f0b0d
Jess <candylee022365@gmail.com>
2024-01-29 23:32:45 +0800

    加入建立優惠券功能

--- production/add-coupon.php
+++ production/add-coupon.php
@@ -6,70 +6,73 @@
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
+    <!-- fontawesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Bootstrap -->
-    <link href="cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
-    <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+
 </head>
 
 <body>
     <div class="container">
-        <div class="py-2">
-            <a class="btn btn-info" href="coupons.php" role="button"><i class="fa-solid fa-chevron-left"></i>返回</a>
-        </div>
-        <form action="#" method="post">
-            <div class="mb-2">
-                <label for="">優惠券名稱</label>
+        <h2 class="my-3">建立優惠券</h2>
+        <form action="doAddCoupon.php" method="post">
+            <div class="mb-3">
+                <label class="form-label" for="">優惠券名稱</label>
                 <input type="text" class="form-control" name="name">
             </div>
-            <div class="col-sm-3">
-                <label class="visually-hidden" for="couponCode">Coupon Code</label>
-                <div class="input-group">
-                    <span class="input-group-text">NUTRI</span>
-                    <input type="text" class="form-control" id="couponCode" placeholder="ABCDE">
-                </div>
+            <div class="mb-3">
+                <label class="form-label" for="couponCode">優惠券代碼</label>
+                    <input type="text" class="form-control" id="couponCode" placeholder="請填入八位英數混合數字" name="code">
             </div>
-            <div class="mb-2">
-                <label for="">優惠券使用時間</label>
-                <input type="tel" class="form-control" name="phone">
-                <input type="tel" class="form-control" name="phone">
-            </div>
-            <div class="row">
-                <div class="form-group col-md-6">
-                    <label for="datePicker">優惠券使用時間</label>
-                    <input type="date" class="form-control" id="datePicker">
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="datePicker"></label>
-                    <input type="date" class="form-control" id="datePicker">
-                </div>
-            </div>
-            <label for="">折扣方式｜折扣額度</label>
-            <div class="row">
 
-                <div class="col-md-6">
-                    <select class="form-select form-select-lg mb-3" aria-label="Large select example">
-                        <option value="1">折扣金額</option>
-                        <option value="2">折扣折數</option>
-                </div>
-                <div class="col-md-6">
-                    <input type="text" class="form-control" id="couponCode" placeholder="NT$">
-                </div>
-                </select>
-            </div>
-            <div class="mb-2">
-                <label for="">Phone</label>
-                <input type="tel" class="form-control" name="phone">
-            </div>
-            <button type="submit" class="btn btn-primary">
-                送出
-            </button>
 
+
+            <div class="row mb-3">
+                <label class="form-label" for="">優惠券使用時間</label>
+                <div class="form-group col-auto">
+                    <input type="date" class="form-control" id="datePicker" name="validStartDate">
+                </div>
+                <div class="col-auto">~</div>
+                <div class="form-group col-auto">
+                    <input type="date" class="form-control" id="datePicker" name="validEndDate">
+                </div>
+            </div>
+
+            <div class="mb-3 row">
+                <label for="" class="form-label">折扣方式</label>
+                <div class="form-check col-auto">
+                    <input class="form-check-input" type="radio" name="discount_type" id="" value="百分比">
+                    <label class="form-check-label" for="flexRadioDefault1">
+                        百分比
+                    </label>
+                </div>
+                <div class="form-check col-auto">
+                    <input class="form-check-input" type="radio" name="discount_type" id="" value="金額" checked>
+                    <label class="form-check-label" for="flexRadioDefault2">
+                        金額
+                    </label>
+                </div>
+            </div>
+            <div class="mb-3">
+                    <label for="" class="form-label">優惠券面額</label>
+                    <input type="text" class="form-control" id="couponCode" placeholder="請填入數字 / 折數，例如300或0.9" name="couponAmount">
+                </div>
+
+            <div class="mb-3">
+                <label for="" class="form-label">最低消費金額</label>
+                <input type="number" class="form-control" name="min_amount">
+            </div>
+            <div class="py-2">
+                <button type="submit" class="btn btn-info me-3" role="button" style="background-color: #17a2b8;color:#fff">確認</button>
+                <a type="" class="btn btn-secondary" href="coupons.php" role="button">取消</a>
+
+            </div>
         </form>
-
     </div>
-    <script>
+
+<!--     <script>
         // 當表單被提交時，確認優惠券碼的格式
         document.querySelector('form').addEventListener('submit', function(event) {
             var couponInput = document.getElementById('couponCode');
@@ -81,7 +84,7 @@
                 event.preventDefault(); // 阻止表單提交
             }
         });
-    </script>
+    </script> -->
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 
