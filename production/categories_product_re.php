76e530f27e31804eae796b650b8fdbe97df4962b
CC <vvvv4577@gmail.com>
2024-01-30 13:35:44 +0800

    移除模板預設搜索

--- production/categories_product_re.php
+++ production/categories_product_re.php
@@ -309,12 +309,12 @@ $product_type_count = $result_all_product->num_rows;
 
               <div class="title_right">
                 <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
-                  <div class="input-group">
+                  <!-- <div class="input-group">
                     <input type="text" class="form-control" placeholder="Search for...">
                     <span class="input-group-btn">
                       <button class="btn btn-secondary" type="button">Go!</button>
                     </span>
-                  </div>
+                  </div> -->
                 </div>
               </div>
             </div>
