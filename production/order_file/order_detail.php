f48bf0ffe18fc7a60cb96eee54acb7426f460af9
CharlesC1999 <vvvv4577@gmail.com>
2024-02-01 05:10:58 +0800

    +返回鍵

--- production/order_file/order_detail.php
+++ production/order_file/order_detail.php
@@ -402,9 +402,9 @@ if (isset($_POST['Order_ID'])) {
                   <div class="x_title">
                     <h2>訂單詳情 <small>Orders detail</small></h2>
                     <ul class="nav navbar-right panel_toolbox">
-
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
+                        <li><a class="collapse-link text-info" href="order.php"><i class="fa fa fa-arrow-left"></i> 返回</a></li>
+                        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                        </li>
                     </ul>
                     <div class="clearfix"></div>
                   </div>
