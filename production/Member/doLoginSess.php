5b291f12384c3870e28c8e4cdd92fb15a6accbdf
Wei <lucas060sss@gmail.com>
2024-01-31 14:04:33 +0800

    檔案路徑更改

---
+++ production/Member/doLoginSess.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+require_once "./connect.php";
+
+if (!isset($_POST["account"])) {
+    echo "請循正常管道進入";
+    exit;
+}
+
+$account = $_POST["account"];
+$password = $_POST["password"];
+$repassword = $_POST["repassword"];
+echo "$acccount,$password,$repassword";
+
+if (empty($acccount)) {
+    $_SESSION["error"]["message"] = "請輸入帳號";
+    header("location:login-sess.php");
+}
+
+if (empty($password)) {
+    $_SESSION["error"]["message"] = "請輸入密碼";
+    header("location:login-sess.php");
+}
+
+if (empty($repassword)) {
+    $_SESSION["error"]["message"] = "請輸入重複的密碼";
+    header("location:login-sess.php");
+}
+
+$password = md5($password);
