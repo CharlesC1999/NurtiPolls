e31befeb04d68237a88f0b5002239e0b8e1cc2cc
Wei <lucas060sss@gmail.com>
2024-02-05 14:42:56 +0800

    新增註解

--- production/Member/add-user.php
+++ production/Member/add-user.php
@@ -365,7 +365,7 @@ require_once "../../db_connect.php";
                 </label>
                 <input type="number" class="form-control" name="phone" required="required">
             </div>
-            <!-- 生日 -->
+
             <div class="mt-2">
                 <!-- 生日 -->
                 <label for="" class="form-label">
@@ -373,8 +373,6 @@ require_once "../../db_connect.php";
                 </label>
                 <input type="date" class="form-control" name="birth" >
             </div>
-
-
             <!--  -->
             <div class="mt-2">
             <label for="" class="form-label">選擇圖片</label>
